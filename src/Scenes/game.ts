31493619ab52354b32a2ceb687f5840551c1f803
Nicolas Espinosa Mooser <nico.mooser@gmail.com>
2024-03-06 19:25:01 +0100

    camera movement updated

--- src/Scenes/game.ts
+++ src/Scenes/game.ts
@@ -10,8 +10,10 @@ const MIN_ZOOM = 0.05;
 const MAX_ZOOM = 1.3;
 const ZOOM_AMOUNT = 0.05;
 const MIN_POS = -64;
+const MOVEMENT_OFFSET = 50;
 
 export default class Game extends Phaser.Scene {
+  private pointerInMap = true;
   private mapId: string;
   private _map: Phaser.Tilemaps.Tilemap;
   private _buildingsLayer: Phaser.GameObjects.GameObject[];
@@ -38,23 +40,23 @@ export default class Game extends Phaser.Scene {
     this._map.createLayer('Fondo/Grass', tileset!);
 
     // Resources
-    this._map.createFromObjects('Resources/Food', { type: "Sheep", key: 'sheep'});
-    this._map.createFromObjects('Resources/Wood', { type: "Tree", key: 'tree'});
-    this._map.createFromObjects('Resources/Gold', { type: "GoldMine", key: 'gold_inactive'});
+    this._map.createFromObjects('Resources/Food', { type: "Sheep", key: 'sheep' });
+    this._map.createFromObjects('Resources/Wood', { type: "Tree", key: 'tree' });
+    this._map.createFromObjects('Resources/Gold', { type: "GoldMine", key: 'gold_inactive' });
 
     // Buildings
     this._buildingsLayer = this._map.createFromObjects('Buildings', [
       { type: "Townhall_Blue", key: 'Townhall_Blue', classType: TownHall},
       { type: "Townhall_Red", key: 'Townhall_Red', classType: TownHall},
-      { type: "Townhall_Purple", key: 'Townhall_Purple', classType: TownHall},
-      { type: "Townhall_Yellow", key: 'Townhall_Yellow', classType: TownHall}
+      // { type: "Townhall_Purple", key: 'Townhall_Purple', classType: TownHall},
+      // { type: "Townhall_Yellow", key: 'Townhall_Yellow', classType: TownHall}
     ]);
 
     const npcLayer = this._map.createFromObjects("NPCs", [
-      { type: "Villager_Blue", key: 'Villager_Blue', classType: villager},
-      { type: "Villager_Red", key: 'Villager_Red', classType: villager},
-      { type: "Villager_Purple", key: 'Villager_Purple', classType: villager},
-      { type: "Villager_Yellow", key: 'Villager_Yellow', classType: villager}
+      { type: "Villager_Blue", key: 'Villager_Blue', classType: villager },
+      { type: "Villager_Red", key: 'Villager_Red', classType: villager },
+      // { type: "Villager_Purple", key: 'Villager_Purple', classType: villager},
+      // { type: "Villager_Yellow", key: 'Villager_Yellow', classType: villager}
     ]);
 
     // Event listener al hacer scroll
@@ -74,12 +76,36 @@ export default class Game extends Phaser.Scene {
       }
     });
 
-    // Event listener al hacer click y mover
-    this.input.on('pointermove', (pointer) => {
-      if (!pointer.isDown) return;
+    this.input.on('gameout', () => this.pointerInMap = false);
+    this.input.on('gameover', () => this.pointerInMap = true);
+  }
 
-      this.cameras.main.scrollX -= (pointer.x - pointer.prevPosition.x) / this.cameras.main.zoom;
-      this.cameras.main.scrollY -= (pointer.y - pointer.prevPosition.y) / this.cameras.main.zoom;
-    });
+  update(time: number, delta: number): void {
+    this.cameraPan(delta)
+  }
+
+  cameraPan(delta: number) {
+    let { width, height } = this.sys.game.canvas;
+    const pointer = this.input.activePointer.position;
+
+    if (pointer.x === 0 && pointer.y === 0)
+      return;
+
+    if (!this.pointerInMap)
+      return;
+
+    if (pointer.x >= width - MOVEMENT_OFFSET)
+      // move right
+      this.cameras.main.scrollX += delta / this.cameras.main.zoom;
+    else if (pointer.x <= MOVEMENT_OFFSET)
+      // move left
+      this.cameras.main.scrollX -= delta / this.cameras.main.zoom;
+
+    if (pointer.y >= height - MOVEMENT_OFFSET)
+      // move down
+      this.cameras.main.scrollY += delta / this.cameras.main.zoom;
+    else if (pointer.y <= MOVEMENT_OFFSET)
+      // move up
+      this.cameras.main.scrollY -= delta / this.cameras.main.zoom;
   }
 }
