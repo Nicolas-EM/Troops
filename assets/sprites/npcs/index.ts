8034f4e2b838a70b57b4f0e8e3ac6404a58e96d2
LucasBravo01 <144778411+LucasBravo01@users.noreply.github.com>
2024-03-11 19:43:23 +0100

    Fixing files (part 57)

---
+++ assets/sprites/npcs/index.ts
@@ -0,0 +1,5 @@
+export * as Archer from './archer';
+export * as Goblin from './goblin';
+export * as Soldier from './soldier';
+export * as Villager from './villager';
+export { default as Dead } from './dead.png';
