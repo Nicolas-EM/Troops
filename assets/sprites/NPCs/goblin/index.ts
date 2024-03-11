1009db9b848b6227f1b2531b6ecefed70019d8ea
LucasBravo01 <144778411+LucasBravo01@users.noreply.github.com>
2024-03-11 19:52:46 +0100

    Fixing files (part 75)

--- assets/sprites/NPCs/goblin/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Blue } from './blue.png';
-export { default as Purple } from './purple.png';
-export { default as Red } from './red.png';
-export { default as Yellow } from './yellow.png';
--- assets/sprites/NPCs/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Archer from './archer';
-export * as Goblin from './goblin';
-export * as Soldier from './soldier';
-export * as Villager from './villager';
-export { default as Dead } from './dead.png';
