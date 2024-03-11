685859a441f2635075b1b502895e01b504c9ee40
Beatriz Espinar <79699132+bea-espinar15@users.noreply.github.com>
2024-03-11 19:26:30 +0100

    Fixing files (part 26)

---
+++ assets/sprites/resources/spawners/gold_mine/index.ts
@@ -0,0 +1,3 @@
+export { default as Active } from './gold_mine_active.png';
+export { default as Inactive } from './gold_mine_inactive.png';
+export { default as Destroyed } from './gold_mine_destroyed.png';
