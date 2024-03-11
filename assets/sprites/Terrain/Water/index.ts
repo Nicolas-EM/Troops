26ac82d59e71915d4ab7bcd737f31af401233bcc
Beatriz Espinar <79699132+bea-espinar15@users.noreply.github.com>
2024-03-11 19:21:30 +0100

    Fixing files (part 19)

--- assets/sprites/Terrain/Water/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Water } from './water.png';
-export { default as Foam } from './foam.png';
-export { default as Rocks } from './rocks.png';
-export { default as Bridge } from './bridge.png';
