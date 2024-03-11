fc3090d111499e637e7ef683886407a1ce2fc788
Beatriz Espinar <79699132+bea-espinar15@users.noreply.github.com>
2024-03-11 19:01:49 +0100

    Fixing files (part 4)

---
+++ src/classes/resources/GoldMine.ts
@@ -0,0 +1,12 @@
+import ResourceSpawner from "./ResourceSpawner"
+
+// TODO: remove magic numbers
+const TOTAL_RESOURCE = 100;
+const RESOURCE_RATE = 10;
+
+export default class GoldMine extends ResourceSpawner {
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture, frame?: string | number) {
+        super(scene, x, y, texture, TOTAL_RESOURCE, RESOURCE_RATE, frame)
+    }
+}
