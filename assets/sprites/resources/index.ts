4b83750bfdb88412142d6ed462fb25bce08eb5a6
Beatriz Espinar <79699132+bea-espinar15@users.noreply.github.com>
2024-03-11 19:23:28 +0100

    Fixing files (part 22)

---
+++ assets/sprites/resources/index.ts
@@ -0,0 +1,10 @@
+export * as Spawners from './spawners';
+export { default as Food_Shadow } from './food_shadow.png';
+export { default as Food_Spawn } from './food_spawn.png';
+export { default as Food } from './food.png';
+export { default as Gold_Shadow } from './gold_shadow.png';
+export { default as Gold_Spawn } from './gold_spawn.png';
+export { default as Gold } from './gold.png';
+export { default as Wood_Shadow } from './wood_shadow.png';
+export { default as Wood_Spawn } from './wood_spawn.png';
+export { default as Wood } from './wood.png';
