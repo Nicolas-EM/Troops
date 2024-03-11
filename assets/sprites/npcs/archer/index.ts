458f432212c290e38f20a8b97a12342cd7e109bd
LucasBravo01 <144778411+LucasBravo01@users.noreply.github.com>
2024-03-11 19:52:15 +0100

    Fixing files (part 73)

---
+++ assets/sprites/npcs/archer/index.ts
@@ -0,0 +1,4 @@
+export { default as Blue } from './blue.png';
+export { default as Purple } from './purple.png';
+export { default as Red } from './red.png';
+export { default as Yellow } from './yellow.png';
