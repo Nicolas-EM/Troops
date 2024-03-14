5d47a7fef6c1a30e5312d44af0849cc41d4a1696
Nicolas-EM <nico.mooser@gmail.com>
2024-03-14 18:45:30 +0100

    ups

--- src/classes/PlayerEntity.ts
+++ src/classes/PlayerEntity.ts
@@ -20,8 +20,10 @@ export default abstract class PlayerEntity extends Phaser.GameObjects.Sprite {
         this._owner = owner;
         this._health = health;
         this._visionRange = visionRange;
+        // Enable arcade physics for moving with velocity
+        this.scene.physics.world.enable(this);
     }
-    
+
     /**
      * @param damage
      */
@@ -32,9 +34,44 @@ export default abstract class PlayerEntity extends Phaser.GameObjects.Sprite {
     goTo(targetPoint: Phaser.Math.Vector2): void {
         // Find a path to the target
         this._path = this._navMesh.findPath(new Phaser.Math.Vector2(this.x, this.y), targetPoint);
-    
-        // If there is a valid path, grab the first point from the path and set it as the target
         if (this._path && this._path.length > 0) this._currentTarget = this._path.shift();
         else this._currentTarget = null;
     }
+
+    update(time: number, deltaTime: number) {
+        if (!this.body) return;
+        this.body.velocity.set(0);
+
+        if (this._currentTarget) {
+            const { x, y } = this._currentTarget;
+            const distance = Phaser.Math.Distance.Between(this.x, this.y, x, y);
+
+            if (distance < 5) {
+                if (this._path.length > 0) this._currentTarget = this._path.shift();
+                else this._currentTarget = null;
+            }
+            let speed = 400;
+            if (this._path.length === 0 && distance < 50) {
+                speed = map(distance, 50, 0, 400, 50);
+            }
+            if (this._currentTarget) this.moveTowards(this._currentTarget, speed, deltaTime / 1000);
+        }
+    }
+
+    moveTowards(targetPosition: Phaser.Math.Vector2, maxSpeed = 200, elapsedSeconds: number) {
+        const { x, y } = targetPosition;
+        const angle = Phaser.Math.Angle.Between(this.x, this.y, x, y);
+        const distance = Phaser.Math.Distance.Between(this.x, this.y, x, y);
+        const targetSpeed = distance / elapsedSeconds;
+        const magnitude = Math.min(maxSpeed, targetSpeed);
+
+        this.scene.physics.velocityFromRotation(angle, magnitude, this.body.velocity);
+        this.rotation = angle;
+    }
+
+    destroy() {
+        if (this.scene) this.scene.events.off("update", this.update, this);
+        super.destroy();
+    }
+}
 }
